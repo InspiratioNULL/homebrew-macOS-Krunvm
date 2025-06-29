c09e4edb01329a86a2e6995131f0702106b53bf8
InspiratioNULL <206951839+InspiratioNULL@users.noreply.github.com>
2025-06-29 03:39:48 -0700

    Update libkrun.rb

--- Formula/libkrun.rb
+++ Formula/libkrun.rb
@@ -5,10 +5,7 @@ class Libkrun < Formula
   sha256 "f213db9d9b436e255dc01aedc6de2767f400d8e16dba2d95b1095aa0e996c9ba"
   license "Apache-2.0"
 
-  bottle do
-    root_url "https://raw.githubusercontent.com/slp/homebrew-krun/master/bottles"
-    sha256 cellar: :any, arm64_ventura: "16e011e8d435e1b400ef1db790094acf47986e60e7f29a9aff39e6de94f43929"
-  end
+ 
 
   depends_on "rust" => :build
   # Upstream only supports Hypervisor.framework on arm64
