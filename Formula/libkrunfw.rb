2df0f7894e34ae232cc1fba740b6c2912a54eae0
InspiratioNULL <206951839+InspiratioNULL@users.noreply.github.com>
2025-06-29 03:40:01 -0700

    Update libkrunfw.rb

--- Formula/libkrunfw.rb
+++ Formula/libkrunfw.rb
@@ -5,10 +5,7 @@ class Libkrunfw < Formula
   sha256 "5dc76c2bc2f93703207a548c844d98298c5a0229ffde9ffe1ab9631866692b96"
   license all_of: ["GPL-2.0-only", "LGPL-2.1-only"]
 
-  bottle do
-    root_url "https://raw.githubusercontent.com/slp/homebrew-krun/master/bottles"
-    sha256 cellar: :any, arm64_ventura: "1c423c2240ef907eca6df8dfdb5e534b2fa7e7c7425f28be4fec83b13e3d0a42"
-  end
+
 
   # libkrun, our only consumer, only supports Hypervisor.framework on arm64
   depends_on arch: :arm64
