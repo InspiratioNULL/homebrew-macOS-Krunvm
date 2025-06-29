6ad51fc9b2cbb13a650a0d5dec0becbc75688a04
InspiratioNULL <206951839+InspiratioNULL@users.noreply.github.com>
2025-06-29 03:39:29 -0700

    Update krunvm.rb

--- Formula/krunvm.rb
+++ Formula/krunvm.rb
@@ -5,10 +5,7 @@ class Krunvm < Formula
   sha256 "b290bcb70f341ac4814b0eb0e3a19084c301135f61e49f406168157b0d5f74c4"
   license "Apache-2.0"
 
-  bottle do
-    root_url "https://raw.githubusercontent.com/slp/homebrew-krun/master/bottles"
-    sha256 cellar: :any, arm64_ventura: "7d9575b060fb423d1ffb26696e810ce2b1a4e159934cfff3761fb66aa1c111ed"
-  end
+  
 
   depends_on "asciidoctor" => :build
   depends_on "rust" => :build
