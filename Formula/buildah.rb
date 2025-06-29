a9108a71a4a63826aeb22528738f0bebe12016ee
InspiratioNULL <206951839+InspiratioNULL@users.noreply.github.com>
2025-06-29 03:40:13 -0700

    Update buildah.rb

--- Formula/buildah.rb
+++ Formula/buildah.rb
@@ -5,10 +5,7 @@ class Buildah < Formula
   sha256 "4e406a0cc6a90066cd471deea252fe8862dbd7fa9cb72b274617673d6159a32b"
   license "Apache-2.0"
 
-  bottle do
-    root_url "https://raw.githubusercontent.com/slp/homebrew-krun/master/bottles"
-    sha256 cellar: :any, arm64_ventura: "62df65e2b7a98f59dd3e683c18992c35144ea780947cb0fc164165329972d878"
-  end
+
 
   depends_on "go" => :build
   depends_on "go-md2man" => :build
